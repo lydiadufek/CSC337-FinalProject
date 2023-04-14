8c4a8ee6f8d4d49f1a56fdc88216bedfeb811d79
joe12334667 <joe12334667@gmail.com>
2023-04-14 14:56:47 -0700

    Revert "add hash to the login api"
    This reverts commit 5e8a33bfd6e794fbd7f400073b9a187b0cf67322.

--- server.js
+++ server.js
@@ -185,13 +185,16 @@ async function startServer() {
         let u = req.params.username;
         let p = req.params.password;
 
-        User.findOne({ username: u}).exec().then((results) => {
-           console.log(results); // single object
-           bcrypt.compare(p, results.hash, function(err, result) {
-            console.log(result);
-            res.end(JSON.stringify({ 'status': result }));
-        });
-    
+        User.find({ username: u, password: p }).exec().then((results) => {
+            if (results.length > 0) {
+                s = addSession(u);
+                res.cookie("login", { 'username': u, 's_id': s }, { maxAge: SESSION_LENGTH });
+                res.end(JSON.stringify({ 'status': 'match' }));
+                console.log("Success!");
+            }
+            else {
+                res.end(JSON.stringify({ 'status': 'not match' }));
+            }
         }).catch((err) => {
             console.log(err);
             res.end("login failed");
