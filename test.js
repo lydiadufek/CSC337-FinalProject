441ce919b446ec77e6ddf706a53d7f3cd4bf30a8
joe12334667 <joe12334667@gmail.com>
2023-04-14 15:00:56 -0700

    update test.js

--- test.js
+++ test.js
@@ -1,4 +1,26 @@
-DBClass = require('./DBClass.js');
-let user = DBClass.user("222", "joe", "joe@email.com", "123", "132", "user");
-console.log("object:" + user);
-console.log("json object:" + JSON.stringify(user));
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const myPlaintextPassword = '123456789';
+const someOtherPlaintextPassword = 'not_bacon';
+
+
+
+bcrypt.hash(myPlaintextPassword, saltRounds, function(err, hash) {
+    console.log("hash2 : "+ hash);
+    bcrypt.compare(someOtherPlaintextPassword, hash, function(err, result) {
+        // result == true
+        console.log(result);
+    });
+
+});
+
+
+
+
+
+// bcrypt.genSalt(saltRounds, function(err, salt) {
+//     bcrypt.hash(myPlaintextPassword, salt, function(err, hash) {
+//         console.log("salt : "+salt);
+//         console.log("hash : "+ hash);
+//     });
+// });
